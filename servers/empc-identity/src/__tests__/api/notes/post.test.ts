cd32e5be9ffe12933d31249dfe7e933aa097feea
Napihuphup <programminghup@gmail.com>
2020-03-30 17:00:08 +0800

    update Tests

--- servers/empc-identity/src/__tests__/api/notes/post.test.ts
+++ servers/empc-identity/src/__tests__/api/notes/post.test.ts
@@ -36,8 +36,8 @@ describe('UserController', () => {
   it('(User Login) : invoke POST /users/login', (done) => {
     client.post('/users/login')
       .send({
-        email: 'empc@gmail.com',
-        password: 'Napi_developer10'
+        userName: 'Hanafi',
+        password: 'admin'
       })
       .then(resp => {
         const body = resp.body;
@@ -85,5 +85,55 @@ describe('UserController', () => {
         }
       })
   })
+
+  /**
+   * To test on feature for Offcier Account Data Creation
+   *
+   * @precondition : User should be a master or admin in order to access to
+   * this api
+   * @FormBody : <OfficerFormCreation>
+   * @Account : Login as 'Hanafi'
+   */
+  it('Create Officer Account ) : invoke POST /users/create/officer', (done) => {
+    client.post('/users/create/officer')
+      .send({
+        userName: 'Jakob',
+        firstName: 'JAKOB',
+        lastName: 'BRANDON',
+        email: 'jakob@empc.mail.com',
+        mobileNumber: '90098743',
+        roles: ['officer'],
+        rights: ['officer'],
+        officerId: '123432',
+        userChoicePassword: 'jakob',
+        userConfirmPassword: 'jakob',
+        zoneId: 'f1645d09-d8ef-4ca6-9bc1-8c9bf3139c41'
+      })
+      .auth("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI4MTg3ODM5Ni00ZDZlLTRhODktYmEwNS0wN2M1MzBjNjA1ZjAiLCJzdWJqZWN0IjoiODE4NzgzOTYtNGQ2ZS00YTg5LWJhMDUtMDdjNTMwYzYwNWYwIiwibmFtZSI6Ik11aGFtbWFkIEhhbmFmaSBZYWt1YiIsInJvbGVzIjpbImFkbWluIl0sInJpZ2h0cyI6WyJhZG1pbmlzdHJhdGlvbiJdLCJpYXQiOjE1ODU1NTgyNTIsImV4cCI6MTU4NTY0NDY1MiwiYXVkIjoiY2xpZW50IiwiaXNzIjoiRU1QQyBJZGVudGl0eSBTZXJ2aWNlIiwic3ViIjoiODE4NzgzOTYtNGQ2ZS00YTg5LWJhMDUtMDdjNTMwYzYwNWYwIn0.E_drPCcdCrSWTPI_EeWUI1o7_9t4jU--F4nZKmDnatm_VoRNANSyTZFb9ubGAGXZAMezuY29YmwN_GoGM2Nlm1gbX5Xeu_pxA9AbOm4Fi5ha0qpCcYEms0YwFE17XmbEVS39IcsNuk3HFWOr1ChqwTv8cy1G-lrxMEdFoniOX3c", { type: 'bearer' })
+
+      .then(response => {
+        const body = response.body;
+        expect(body).to.properties('officerId', 'zoneId');
+        done();
+      })
+      .catch(err => {
+        let errorMessage = err.actual.error.message;
+        switch (errorMessage) {
+          case 'Officer ID already being used -  please use different ID': {
+            done();
+            break;
+          }
+          case 'User with username/email already exist in our database': {
+            done()
+            break;
+          }
+          default: done(err); break;
+        }
+      })
+  })
+
+
 })
+
+
 
